03b38fa9ff19b7a3dc053ceb6fe8c9f31e8a3dc8
Michael Cui <Michaelcui20@gmail.com>
2019-11-16 15:24:43 -0800

    deleted useless stuff + updated master/slave code

--- Master_Comm/Master_Comm.ino
+++ Master_Comm/Master_Comm.ino
@@ -1,16 +1,25 @@
 const int baud_rate = 9600;
 
+// pin for button reading
 const int button_pin = 2;
+// pin for the potentiometer reading
 const int pot_pin = 0;
+// output pin for led indicating if pot is turned to right position
 const int pot_led_pin = 7;
+// output pin for led indicating if slave has right pressure
 const int slave_led_pin = 6;
 
+// high if the button is pressed
 int button_state = 0;
-int slave_state = 0;
+// high if slave has right internal pressure
+char slave_state = '0';
+// high if pot is turned to right position
+int pot_state = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  
+  Serial.begin(baud_rate);
+  // pin set up
   pinMode(button_pin, INPUT);
   pinMode(pot_pin, INPUT);
 
@@ -19,21 +28,36 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // Serial.write(i);
-  // i++;
-  // delay(500);
-  // if (i == 255){i = 0;}
+  
+  // reads if the button has been pushed
   button_state = digitalRead(button_pin);
-
+  // read from slave
   if(Serial.available() > 0){slave_state = Serial.read();}
 
-  if (analogRead(pot_pin) >= 1000) {digitalWrite(pot_led_pin, HIGH);}
-  else {digitalWrite(pot_led_pin, LOW);}
+  if(analogRead(pot_pin) >= 1000) {pot_state = HIGH;
+  } else {pot_state = LOW;}
 
-  if(button_state == HIGH){
+  // if pot is turned right position, turn on LED
+  if (pot_state == HIGH) {digitalWrite(pot_led_pin, HIGH);
+  } else {digitalWrite(pot_led_pin, LOW);}
+
+  // if slave pressure reading is corret, turn on LED
+  if(slave_state == '1'){digitalWrite(slave_led_pin, HIGH); 
+  } else {digitalWrite(slave_led_pin, LOW);}
+
+  // if button is pressed, slave at right pressure, and pot at right position launch rocket
+  if(button_state == HIGH && slave_state == '1' && pot_state == HIGH){
+    // send signal to slave to launch the thing
+    Serial.write('2');
+
+    // turn off the LEDs and set the states to low
     digitalWrite(pot_led_pin, LOW);
     digitalWrite(slave_led_pin, LOW);
-    delay(4000);
+    slave_state = '0';
+    // 5 sec delay
+    delay(5000);
+
+    Serial.write('0');
   }
+  delay(10);
 }
--- Slave_Comm/Slave_Comm.ino
+++ Slave_Comm/Slave_Comm.ino
@@ -1,18 +1,47 @@
-int incomingByte = 0;
+const int baud_rate = 9600;
+
+// set up pins
+const int relay_pin = 0;
+const int press_int_pin = A0;
+const int press_ext_pin = A1;
+
+// initialize pressure readings
+double press_int_val = 0;
+double press_ext_val = 0;
+double desired_press_int = 0;
+
+char master_state = '0';
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(96000);
 
+  // set up pressure reading
+  pinMode(press_int_pin, INPUT);
+  pinMode(press_ext_pin, INPUT);
+  // set up relay output pin
+  pinMode(relay_pin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available() > 0){
-    incomingByte = Serial.read();
+  press_int_val = bits_to_psi(analogRead(press_int_pin));
+  press_ext_val = bits_to_psi(analogRead(press_ext_pin));
+  
+  // read from master
+  if(Serial.available() > 0){master_state = Serial.read();}
 
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
-  }
+  // send signal to master that the right pressure has been reached
+  if(press_int_val >= desired_press_int){Serial.write('1');}
 
+  // if master is ready to launch rocket, open the solenoid valve
+  if(master_state = '1'){digitalWrite(relay_pin, HIGH);
+  } else {digitalWrite(relay_pin, LOW);}
+
+  delay(10);
+}
+
+// function that takes pressure reading in bits and outputs psi
+double bits_to_psi(double bits){
+  double voltage = bits*5/1023.0;
+  double psi = voltage *50 - 25;
+  return psi;
 }
