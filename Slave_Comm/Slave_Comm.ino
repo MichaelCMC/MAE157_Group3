d880e38ed032898b02a9fe78982392fc963ef679
AchilleH <56404156+AchilleH@users.noreply.github.com>
2019-11-19 13:24:13 -0800

    Update Slave_Comm.ino

--- Slave_Comm/Slave_Comm.ino
+++ Slave_Comm/Slave_Comm.ino
@@ -1,3 +1,8 @@
+#include <SPI.h>
+#include <SD.h>
+
+const int chipSelect = 4;
+
 const int baud_rate = 9600;
 
 // set up pins
@@ -12,9 +17,22 @@ double desired_press_int = 0;
 
 char master_state = '0';
 
+/////////////////////////////////////////////////////////////////
+//////////////////////////End Header/////////////////////////////
+/////////////////////////////////////////////////////////////////
 void setup() {
   Serial.begin(96000);
+  
+  Serial.print("Initializing SD card...");
 
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.write("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
+  
   // set up pressure reading
   pinMode(press_int_pin, INPUT);
   pinMode(press_ext_pin, INPUT);
@@ -26,6 +44,37 @@ void loop() {
   press_int_val = bits_to_psi(analogRead(press_int_pin));
   press_ext_val = bits_to_psi(analogRead(press_ext_pin));
   
+  ////////////////////////////////////////////////////////
+  /////////////////SD CARD DATA Recording/////////////////
+  ////////////////////////////////////////////////////////
+  String dataString = "";
+
+  // read and record pressure data to a single line
+  //time,internal P, external P
+  //typecasting the data
+  dataString += String(millis()/1000000.0);
+  dataString += ",";
+  dataString += String(press_int_val);
+  dataString += ",";
+  dataString += String(press_ext_val);
+  
+  File dataFile = SD.open("Pressure.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.write(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+  
+  /////////////////////////////////////////////////////////
+  ///////////////////Signal Processing/////////////////////
+  /////////////////////////////////////////////////////////
   // read from master
   if(Serial.available() > 0){master_state = Serial.read();}
 
@@ -35,9 +84,10 @@ void loop() {
   // if master is ready to launch rocket, open the solenoid valve
   if(master_state = '1'){digitalWrite(relay_pin, HIGH);
   } else {digitalWrite(relay_pin, LOW);}
-
-  delay(10);
+  
+  delay(10); //TODO: Replace delay with proper sampling control method
 }
+
 
 // function that takes pressure reading in bits and outputs psi
 double bits_to_psi(double bits){
