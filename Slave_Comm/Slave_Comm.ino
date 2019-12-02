d0a2b02bfce7e2507232e12e2d0374d03367df56
AchilleH <achillehebertiii@gmail.com>
2019-12-02 15:26:30 -0800

    fixed display to take no arguments, added tlaunch to track launch time. Removed unnecesary print statements

--- Slave_Comm/Slave_Comm.ino
+++ Slave_Comm/Slave_Comm.ino
@@ -5,16 +5,17 @@ double bits_to_psi(double bits);
 double tim(double offset = 0);
 void record(double data);
 void dump();
-void Display(double press_int_val, double press_ext_val, char sig);
+void Display();
+float truncate(float val, byte dec);
 
-const int baud_rate = 9600;
+const int baud_rate = 115200;
 // SD Card
 const int chipSelect = 4;
 const String filename = "Pressure.csv";
 String sdata = "";
 
 // set up pins
-const int relay_pin = 0;
+const int relay_pin = 2;
 const int press_int_pin = A0;
 const int press_ext_pin = A1;
 
@@ -29,13 +30,15 @@ const double sam(1000); // sampling frequency in hz
 const int final(10000); //number of total samples to record (altered from 1000000 to 10000)
 const double interval(1.0/sam); //time interval between samples in [s]
 double t(0); // sampling time tracking variable
+double tlaunch(0);
 //signal rate
-const double sig(100); //signal frequency in hz
+const double sig(2); //signal frequency in hz
 const double sInt(1.0/sig); //signal time period in seconds
 double sT(0); //signal time tracking variable
 // Communication Variables
 bool state(false);
 char master_state = '0';
+char current('0');
 
 int i = 0;
 double i_interval = 0;
@@ -44,14 +47,10 @@ double i_interval = 0;
 
 void setup() {
   Serial.begin(baud_rate);
-
-  Serial.print("Initializing SD card...");
   // see if the card is present and can be initialized, keep waiting if not
   while (!SD.begin(chipSelect)) {
-    Serial.write("Card failed, or not present");
     delay(1000);
   }
-  Serial.println("card initialized.");
   // set up pressure reading
   pinMode(press_int_pin, INPUT);
   pinMode(press_ext_pin, INPUT);
@@ -70,17 +69,16 @@ void loop() {
       master_state = Serial.read();
     }
     // send signal to master that the right pressure has been reached
-    if(press_int_val >= desired_press_int){Serial.write('1');}
+    if(press_int_val >= desired_press_int){current = '1';}
 
-    if(master_state = '2')
+    if(tim() - sT >= sInt){
+      Display();
+      sT = tim();
+    }
+
+    if(master_state = 'y')
       state = true
   }
-    // if master is ready to launch rocket, open the solenoid valve
-//    if(master_state = '2') // changed this from a 1 to a 2
-//      digitalWrite(relay_pin, HIGH);
-//    else
-//      digitalWrite(relay_pin, LOW);
-//  }
 
   ///////////////////Launch and Collect Data loop
   while(state == true){
@@ -92,6 +90,7 @@ void loop() {
     //Relay control setup
     if(i*interval >= 2.0)
       digitalWrite(relay_pin, HIGH);
+      tlaunch = tim();
     if(i*interval >= 3.5 && i_interval <= 4.0)
       digitalWrite(relay_pin, LOW);
 
@@ -102,15 +101,17 @@ void loop() {
     press_int_val = bits_to_psi(analogRead(press_int_pin));
     press_ext_val = bits_to_psi(analogRead(press_ext_pin));
     if(i < final && tim() - t >= interval){
-      record(tim());
+      record(tim(tlaunch));
       record(press_int_val);
       record(press_ext_val);
+      sdata += '\n'
+
       t = tim();
       i++;
     }
     if(tim() - sT >= sInt){
       dump(); //dump function for writing to SD card and clearing out sdata
-      Display(press_int_val, press_ext_val, current);
+      Display();
       sT = tim();
     }
   }
@@ -138,27 +139,45 @@ void dump(){
   File dataFile = SD.open(filename, FILE_WRITE);
   // if the file is available, write to it:
   if (dataFile) {
-    dataFile.println(sdata);
+    dataFile.print(sdata);
     dataFile.close();
     sdata = "";
   }
   // if the file isn't open, pop up an error:
   else {
     // Put some kind of error message signal
-    Serial.print("File unable to be opened");
   }
 }
 //Takes pressure readings, typecasts them and appends respective variables beforehand
-void Display(double p1, double p2, char current = '0'){
+void Display(){
   String out = "";
   out += 'a';
-  out += sig;
+  out += current;
   out += 'b';
-  out += String(p2); // external pressure
+  out += String(truncate(press_int_val,2)); // internal pressure
   out += 'c';
-  out += String(p1); // internal pressure
+  out += String(truncate(press_ext_val,2)); // external pressure
   out += 'd';
-  out += String(time());
+  out += String(truncate(time(), 2));
   out += 'e';
-  Serial.println(out); //Serial.write(out);
+  //Serial.println(out);
+  //c_array = out.c_str();
+  char c_array[30] = "";
+  for (i = 0; i < out.length(); i++)
+      c_array[i] = out[i];// comment back in
+  //char c_array[22] = "a0b0.123c0.234d0.345e"; // comment this out
+  Serial.write(c_array);
+}
+
+float truncate(float val, byte dec)
+{
+    float x = val * pow(10, dec);
+    float y = round(x);
+    float z = x - y;
+    if ((int)z == 5)
+    {
+        y++;
+    } else {}
+    x = y / pow(10, dec);
+    return x;
 }
